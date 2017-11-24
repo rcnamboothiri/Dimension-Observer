a3bca953ca454aaef848bc8fc7f6d96f88d19821
Rohit Namboothiri <rohitchandran88@gmail.com>
2017-11-24 12:21:59 +0530

    Inital commit for including files

---
+++ DimensionObserver/MainForm.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace DimensionObserver
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(7, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Folder Path :";
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "FIle FIlter";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(10, 60);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(236, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(10, 177);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(374, 95);
+            this.listBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(265, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(265, 108);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Start Monitoring";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(15, 123);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(439, 319);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "Dimension Observer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ DimensionObserver/MainForm.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace DimensionObserver
+{    
+    public partial class MainForm : Form
+    {
+        public const String startMonitoring = "Start Monitoring";
+        public const String stopMonitoring = "Stop Monitoring";
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void folderBrowserDialog1_HelpRequest_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Create FolderBrowserDialog object.
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            // Show a button to create a new folder.
+            folderBrowserDialog.ShowNewFolderButton = true;
+            DialogResult dialogResult = folderBrowserDialog.ShowDialog();
+            // Get selected path from FolderBrowserDialog control.
+            if (dialogResult == DialogResult.OK)
+            {
+                textBox1.Text = folderBrowserDialog.SelectedPath;
+                Environment.SpecialFolder root = folderBrowserDialog.RootFolder;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Create a new FileSystemWatcher object.
+            FileSystemWatcher fsWatcher = new FileSystemWatcher();
+            switch (button2.Text)
+            {
+                // Start Monitoring…
+                case startMonitoring:
+                    if (!textBox1.Text.Equals(String.Empty))
+                    {
+                        listBox1.Items.Add("Started FileSystemWatcher Service…");
+                        fsWatcher.Path = textBox1.Text;
+                        // Set Filter.
+                        fsWatcher.Filter = (textBox2.Text.Equals(String.Empty)) ? "*.*" : textBox2.Text;
+                        // Monitor files and subdirectories.
+                        fsWatcher.IncludeSubdirectories = true;
+                        // Monitor all changes specified in the NotifyFilters.
+                        fsWatcher.NotifyFilter = NotifyFilters.Attributes |
+                                                 NotifyFilters.CreationTime |
+                                                 NotifyFilters.DirectoryName |
+                                                 NotifyFilters.FileName |
+                                                 NotifyFilters.LastAccess |
+                                                 NotifyFilters.LastWrite |
+                                                 NotifyFilters.Security |
+                                                 NotifyFilters.Size;
+                        fsWatcher.EnableRaisingEvents = true;
+                        // Raise Event handlers.
+                        // fsWatcher.Changed += new FileSystemEventHandler(OnChanged);
+                        fsWatcher.Created += new FileSystemEventHandler(OnCreated);
+                       // fsWatcher.Deleted += new FileSystemEventHandler(OnDeleted);
+                       // fsWatcher.Renamed += new RenamedEventHandler(OnRenamed);
+                       // fsWatcher.Error += new ErrorEventHandler(OnError);
+                        button2.Text = stopMonitoring;
+                        textBox1.Enabled = false;
+                        textBox2.Enabled = false;
+                    }
+                    else
+                    {
+                        listBox1.Items.Add("Please select folder to monitor….");
+                    }
+                    break;
+                // Stop Monitoring…
+                case stopMonitoring:
+                default:
+                    fsWatcher.EnableRaisingEvents = false;
+                    fsWatcher = null;
+                    button2.Text = startMonitoring;
+                    textBox1.Enabled = true;
+                    textBox2.Enabled = true;
+                    listBox1.Items.Add("Stopped FileSystemWatcher Service…");
+                    break;
+            }
+
+        }
+        // FileSystemWatcher – OnCreated Event Handler
+        public void OnCreated(object sender, FileSystemEventArgs e)
+        {
+            // Add event details in listbox.
+            this.Invoke((MethodInvoker)delegate { listBox1.Items.Add("--------------------------------"); });
+            this.Invoke((MethodInvoker)delegate { listBox1.Items.Add(String.Format("Received Filename: {1}", e.FullPath,e.Name, e.ChangeType));});
+           
+            UpdateDatabase(sender,e);
+        }
+
+        public void UpdateDatabase(object sender, FileSystemEventArgs e)
+        {
+            // Add event details in listbox.
+             this.Invoke((MethodInvoker)delegate { listBox1.Items.Add(String.Format("Processing Filename: {1}", e.FullPath, e.Name, e.ChangeType)); });
+            SqlConnection con;
+
+            string sqlconn;
+
+            sqlconn = ConfigurationManager.ConnectionStrings["SqlCom"].ConnectionString;
+            con = new SqlConnection(sqlconn);
+
+            //Creating object of datatable  
+            DataTable tblcsv = new DataTable();
+            //creating columns  
+            tblcsv.Columns.Add("sl no");
+            tblcsv.Columns.Add("Dimension");
+
+            //getting full file path of Uploaded file  
+            string CSVFilePath = e.FullPath;
+            this.Invoke((MethodInvoker)delegate { listBox1.Items.Add(String.Format("FullPath: {0}", e.FullPath, e.Name, e.ChangeType)); });
+            //Reading All text  
+            string ReadCSV = File.ReadAllText(CSVFilePath);
+            //spliting row after new line  
+            foreach (string csvRow in ReadCSV.Split('\n'))
+            {
+                if (!string.IsNullOrEmpty(csvRow))
+                {
+                    //Adding each row into datatable  
+                    tblcsv.Rows.Add();
+                    int count = 0;
+                    foreach (string FileRec in csvRow.Split(','))
+                    {
+                        tblcsv.Rows[tblcsv.Rows.Count - 1][count] = FileRec;
+                        count++;
+                    }
+                }
+
+
+            }
+            //Calling insert Functions  
+            //creating object of SqlBulkCopy    
+            SqlBulkCopy objbulk = new SqlBulkCopy(con);
+            //assigning Destination table name    
+            objbulk.DestinationTableName = "Dimension";
+            //Mapping Table column    
+            objbulk.ColumnMappings.Add("sl no", "sl no");
+            objbulk.ColumnMappings.Add("Dimension", "Dimension");
+
+            //inserting Datatable Records to DataBase    
+            con.Open();
+            objbulk.WriteToServer(tblcsv);
+            con.Close();
+            this.Invoke((MethodInvoker)delegate { listBox1.Items.Add(String.Format("--------------------------------")); });
+        }
+    }
+}
